52870f5c812d7c9939b3a0ef4721268bee1933fb
pendemic <mikeomskinner@aol.com>
2020-12-23 22:49:43 -0800

    Added Data Layer

--- Smartender.UI.MVC/Startup.cs
+++ Smartender.UI.MVC/Startup.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.Owin;
-using Owin;
+﻿using Owin;
 
-[assembly: OwinStartupAttribute(typeof(Smartender.UI.MVC.Startup))]
 namespace Smartender.UI.MVC
 {
     public partial class Startup
