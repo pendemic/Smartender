4027dbf043dec14fee06c751b28b7131fa482f29
pendemic <mikeomskinner@aol.com>
2020-12-23 22:36:45 -0800

    First Commit

---
+++ Smartender.UI.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Smartender.UI.MVC.Startup))]
+namespace Smartender.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
