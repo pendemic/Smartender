c6f52ceb4c0ada28f50047a12b6a286c90436e59
pendemic <mikeomskinner@aol.com>
2020-12-26 17:07:57 -0800

    Added view for cocktails

---
+++ Smartender.UI.MVC/Controllers/CocktailsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Smartender.DATA.EF;
+
+namespace Smartender.UI.MVC.Controllers
+{
+    public class CocktailsController : Controller
+    {
+        private SmartenderEntities db = new SmartenderEntities();
+
+        // GET: Cocktails
+        public ActionResult Index()
+        {
+            var cocktails = db.Cocktails.Include(c => c.Alcohol);
+            return View(cocktails.ToList());
+        }
+
+        // GET: Cocktails/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cocktail cocktail = db.Cocktails.Find(id);
+            if (cocktail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cocktail);
+        }
+
+        // GET: Cocktails/Create
+        public ActionResult Create()
+        {
+            ViewBag.AlcReqID = new SelectList(db.Alcohols, "ID", "Type");
+            return View();
+        }
+
+        // POST: Cocktails/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Name,DrinkID,AlcReqID,Link,Image")] Cocktail cocktail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cocktails.Add(cocktail);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AlcReqID = new SelectList(db.Alcohols, "ID", "Type", cocktail.AlcReqID);
+            return View(cocktail);
+        }
+
+        // GET: Cocktails/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cocktail cocktail = db.Cocktails.Find(id);
+            if (cocktail == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AlcReqID = new SelectList(db.Alcohols, "ID", "Type", cocktail.AlcReqID);
+            return View(cocktail);
+        }
+
+        // POST: Cocktails/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Name,DrinkID,AlcReqID,Link,Image")] Cocktail cocktail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cocktail).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AlcReqID = new SelectList(db.Alcohols, "ID", "Type", cocktail.AlcReqID);
+            return View(cocktail);
+        }
+
+        // GET: Cocktails/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cocktail cocktail = db.Cocktails.Find(id);
+            if (cocktail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cocktail);
+        }
+
+        // POST: Cocktails/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Cocktail cocktail = db.Cocktails.Find(id);
+            db.Cocktails.Remove(cocktail);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
